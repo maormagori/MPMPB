d764d146b9f6a97bb272d4a8e46391a9b56bec61
Maor Magori <maor@magori.online>
2021-11-11 16:15:42 +0200

    some basic config

---
+++ config/config.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+let config = {
+    env: process.env.NODE_ENV || "development",
+    port: process.env.PORT || "5588",
+    hostname: process.env.HOSTNAME || "127.0.0.1",
+};
