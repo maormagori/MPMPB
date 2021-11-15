31050f10ae9853654011a87bc9b0743a6f4e1cb2
Maor Magori <maor@magori.online>
2021-11-15 15:10:49 +0200

    refactor: 🔧 Add syncer configuration to main config

--- config/config.js
+++ config/config.js
@@ -4,6 +4,24 @@ let config = {
     env: process.env.NODE_ENV || "development",
     port: process.env.PORT || "5588",
     hostname: process.env.HOSTNAME || "127.0.0.1",
+
+    syncer: {
+        trello: {
+            isWorking: false,
+            latest: {
+                card: "",
+                timestamp: null,
+            },
+        },
+
+        github: {
+            isWorking: false,
+            latest: {
+                issue: "",
+                timestamp: null,
+            },
+        },
+    },
 };
 
 module.exports = config;
