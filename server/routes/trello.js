0bd12c57daa132e0159182d079c7468721ac446c
Maor Magori <maor@magori.online>
2021-11-15 18:55:27 +0200

    feat: 🎨 trello trigger now respondes when promise finished
    add a failing response in the future

--- server/routes/trello.js
+++ server/routes/trello.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const trello = require("../services/trello");
 
 router.post("/update/:id", (req, res) => {
-    trello.cardsTrigger(req.body);
+    trello.cardsTrigger(req.body).then(send());
 });
 
 module.exports = router;
