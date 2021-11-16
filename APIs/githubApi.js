b07916c835e0ee974643cbda727f311cc5b38aa5
Maor Magori <maor@magori.online>
2021-11-16 13:01:38 +0200

    feat: ✨ Add github api functions

---
+++ APIs/githubApi.js
@@ -0,0 +1,39 @@
+const { Octokit } = require("@octokit/rest");
+const { auth } = require("../config/config");
+
+const githubApi = new Octokit({
+    auth: auth.github.token,
+    userAgent: "MPMPB",
+});
+
+const createAnIssueFromTrelloCard = async (card, repo) => {
+    let githubRes = await githubApi.rest.issues.create({
+        repo: repo,
+        owner: auth.github.user,
+        title: card.title,
+        body: card.description,
+    });
+
+    return githubRes;
+};
+
+const updateAnIssueFromTrelloCard = async (card, repo, issue_number) => {
+    let githubRes = await githubApi.rest.issues.update({
+        owner: auth.github.user,
+        repo: repo,
+        issue_number: issue_number,
+        title: card.title,
+        body: card.description,
+        state: card.state,
+    });
+
+    return githubRes;
+};
+
+const getIssue = async (issue_number, repo) => {
+    return await githubApi.rest.issues.get({
+        owner: auth.github.user,
+        repo: repo,
+        issue_number: issue_number,
+    });
+};
