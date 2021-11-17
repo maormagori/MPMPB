c74da9aca01c61103efe2a18a99f2ab16e4b3622
Maor Magori <maor@magori.online>
2021-11-17 17:04:31 +0200

    fix: 🎨 Change syncer trello service enpoint to include project name

--- server/routes/trello.js
+++ server/routes/trello.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 const trello = require("../services/trello");
 
 router.post("/:project/:githubRepo", (req, res) => {
-    trello.cardsTrigger(req.body, req.params.githubRepo).then(send());
+    trello
+        .cardsTrigger(req.body, req.params.githubRepo, req.params.githubRepo)
+        .then(send());
 });
 
 module.exports = router;
