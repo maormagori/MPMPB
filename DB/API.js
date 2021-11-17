bbe1f4fa02b57d01af3641c8d4837783c82da23f
Maor Magori <maor@magori.online>
2021-11-17 16:10:40 +0200

    fix: 🗃️ Changed table name placeholder to just the table name.
    Table name can't be a placeholder so It's replaced with the table name as a string.

--- DB/API.js
+++ DB/API.js
@@ -2,11 +2,15 @@ const sqlite3 = require("better-sqlite3");
 const db = new sqlite3("syncer.db", { verbose: console.log });
 
 const insertNewCardIssuePair = (project, cardId, issueNumber) => {
+    _createSyncerTableIfNotExists(project);
+
     let result = false;
     try {
         const info = db
-            .prepare("INSERT INTO ? (card_id, issue_number) VALUES (?, ?)")
-            .run(project, cardId, issueNumber);
+            .prepare(
+                `INSERT INTO ${project} (card_id, issue_number) VALUES (?, ?)`
+            )
+            .run(cardId, issueNumber);
         if (info.changes > 0) {
             result = true;
         }
@@ -23,16 +27,28 @@ const insertNewCardIssuePair = (project, cardId, issueNumber) => {
 
 const getIssueNumberFromCardId = (project, cardId) => {
     const issueNumber = db
-        .prepare("SELECT issue_number FROM ? WHERE card_id = ?")
-        .get(project, cardId);
+        .prepare(`SELECT issue_number FROM ${project} WHERE card_id = ?`)
+        .get(cardId);
 
     return issueNumber;
 };
 
 const getCardIdFromIssueNumber = (project, issueNumber) => {
     const cardId = db
-        .prepare("SELECT card_id FROM ? WHERE issue_number = ?")
-        .get(project, issueNumber);
+        .prepare(`SELECT card_id FROM ${project} WHERE issue_number = ?`)
+        .get(issueNumber);
 
     return cardId;
+};
+
+const _createSyncerTableIfNotExists = (tableName) => {
+    db.prepare(
+        `CREATE TABLE IF NOT EXISTS ${tableName} (card_id TEXT NOT NULL UNIQUE, issue_number INTEGER NOT NULL)`
+    ).run();
+};
+
+module.exports = {
+    insertNewCardIssuePair,
+    getCardIdFromIssueNumber,
+    getIssueNumberFromCardId,
 };
